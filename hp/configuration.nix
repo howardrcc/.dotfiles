ebc1051dbb67f3438d274b9273ec1a027b479453
howardrcc <howardchingchung@gmail.com>
2022-07-06 21:01:48 +0200

    hp grafana reverse proxy

--- hp/configuration.nix
+++ hp/configuration.nix
@@ -8,7 +8,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-       <home-manager/nixos> 
+      ./grafana.nix
+      <home-manager/nixos> 
     ];
   
 
